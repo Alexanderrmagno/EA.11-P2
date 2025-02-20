d2572c785fd7e027aba20f01dcb8d191e981d0b3
Alexanderrmagno <22192@portalcedi.edu.mx>
2025-02-20 08:55:24 -0600

    commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'password',
+    loadComponent: () => import('./password/password.page').then( m => m.PasswordPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'forgot',
+    loadComponent: () => import('./password/forgot/forgot.page').then( m => m.ForgotPage)
+  },
+  {
+    path: 'logout',
+    loadComponent: () => import('./logout/logout.page').then( m => m.LogoutPage)
+  },
 ];
